d5f44ad70dee9d3596f6b95c8e5bc9d6f61ac21a
SivaSundarP <sivasundarpranavam@gmail.com>
2025-06-18 18:21:11 +0530

    Save data and generate HTML for payment summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,6 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
+import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 
 // MVC: Model View Controller
 renderOrderSummary();
+renderPaymentSummary();
